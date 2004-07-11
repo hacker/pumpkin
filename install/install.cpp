9bd9ea789778ff25ba61a22bca0eacc14ae47cb7
Michael Krelin <hacker@klever.net>
2004-07-11 04:26:34 +0000

    prepared for pumpkin 2.5 release
    git-svn-id: http://svn.klever.net/kin/pumpkin/trunk@6 fe716a7a-6dde-0310-88d9-d003556173a8

--- install/install.cpp
+++ install/install.cpp
@@ -6,8 +6,8 @@
 
 BOOL Install(void)
 {
-STRING dPath = strFETCH_REG_KEY(HKEY_LOCAL_MACHINE,"Software\\Klever Group, Inc.",SKINAME "Path");
-STRING kPath = strFETCH_REG_KEY(HKEY_LOCAL_MACHINE,"Software\\Klever Group, Inc.","KINPath");
+STRING dPath = strFETCH_REG_KEY(HKEY_LOCAL_MACHINE,"Software\\Klever Group",SKINAME "Path");
+STRING kPath = strFETCH_REG_KEY(HKEY_LOCAL_MACHINE,"Software\\Klever Group","KINPath");
 LPCSTR qPath = ((LPCSTR)dPath)?(LPCSTR)dPath:(((LPCSTR)kPath)?(LPSTR)kPath:"C:\\Program Files\\Klever\\Nothings");
 STRING path = REQUESTPATH("  " KINAME,"\nEnter destination path:",qPath);
 	if(!path)
@@ -34,10 +34,10 @@ STRING shortPath = GET_SHORT_PATH(path);
 		MessageBox(NULL,"Failed to install " KINAME " in specified directory",NULL,MB_ICONERROR|MB_OK);
 		return FALSE;
 	}
-	ADDMENU("Accessories\\Klever Co.",SKINAME,path, SKINAME ".exe");
+	ADDMENU("Klever Group",SKINAME,path, SKINAME ".exe");
 
-	strSET_REG_KEY(HKEY_LOCAL_MACHINE,"Software\\Klever Group, Inc.",SKINAME "Path",path);
-	strSET_REG_KEY(HKEY_LOCAL_MACHINE,"Software\\Klever Group, Inc.","KINPath",path);
+	strSET_REG_KEY(HKEY_LOCAL_MACHINE,"Software\\Klever Group",SKINAME "Path",path);
+	strSET_REG_KEY(HKEY_LOCAL_MACHINE,"Software\\Klever Group","KINPath",path);
 
 FILE* inf=CREATE_INF_FILE(path,SKINAME ".INF");
 	if(!inf){
@@ -55,13 +55,13 @@ FILE* inf=CREATE_INF_FILE(path,SKINAME ".INF");
 	INF_FILE_SECTION(inf,"kReg");
 	INF_UNINSTALL_REG(inf,SKINAME);
 	INF_FILE_SECTION(inf,"kMenu");
-	INF_MENU_GROUP(inf,1,"Accessories\\Klever Co.");
+	INF_MENU_GROUP(inf,1,"Klever Group");
 	INF_MENU_ITEM(inf,1,SKINAME);
 	fclose(inf);
 
 	REG_UNINSTALL_COMMAND(SKINAME,"Klever " KINAME,shortPath,SKINAME ".INF","Uninstall");
 
-	MessageBox(NULL,KINAME " installed successfully, you may now run it from Programs/Accessories/Klever Co. menu or remove it using Control Panel Add/Remove Programs applet."," Rejoice!",MB_ICONINFORMATION|MB_OK);
+	MessageBox(NULL,KINAME " installed successfully, you may now run it from 'Programs/Klever Group' menu or remove it using Control Panel Add/Remove Programs applet."," Rejoice!",MB_ICONINFORMATION|MB_OK);
 
 	return TRUE;
 }
